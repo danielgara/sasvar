af3f0c6e43a3eae0c1d9439255509f335901bd55
Daniel Correa <yo@danielgara.com>
2023-11-09 16:58:11 -0500

    imp: categories

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home.index'),
-    path('about', views.about, name='home.about'),
+    path('acerca', views.about, name='home.about'),
+    path('categorias', views.categories, name='home.categories'),
+    path('categorias/papel-carton', views.c1, name='home.categories.c1'),
+    path('categorias/empaques-envoltorios', views.c2, name='home.categories.c2'),
+    path('categorias/residuos-no-aprovechables', views.c3, name='home.categories.c3'),
+    path('categorias/organicos', views.c4, name='home.categories.c4'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,14 +3,70 @@ from django.shortcuts import render
 
 def index(request):
     viewData = {}
+    viewData["title"] = "SASVAR"
     viewData["breadcrumb"] = False
     return render(request, 'index.html', {"viewData": viewData})
 
 
 def about(request):
     viewData = {}
+    viewData["title"] = "Acerca de Nosotros"
     viewData["breadcrumbItems"] = [
-        {"name":"Inicio", "route":"home.index"},
-        {"name":"Acerca", "route":"home.about"},
+        {"name": "Inicio", "route": "home.index"},
+        {"name": "Acerca", "route": "home.about"},
     ]
     return render(request, 'about.html', {"viewData": viewData})
+
+
+def categories(request):
+    viewData = {}
+    viewData["title"] = "Categorías"
+    viewData["breadcrumbItems"] = [
+        {"name": "Inicio", "route": "home.index"},
+        {"name": "Categorías", "route": "home.categories"},
+    ]
+    return render(request, 'categories/index.html', {"viewData": viewData})
+
+
+def c1(request):
+    viewData = {}
+    viewData["title"] = "Papel y cartón limpios"
+    viewData["breadcrumbItems"] = [
+        {"name": "Inicio", "route": "home.index"},
+        {"name": "Categorías", "route": "home.categories"},
+        {"name": "Papel y cartón limpios", "route": "home.categories.c1"},
+    ]
+    return render(request, 'categories/paper.html', {"viewData": viewData})
+
+
+def c2(request):
+    viewData = {}
+    viewData["title"] = "Empaques y envoltorios"
+    viewData["breadcrumbItems"] = [
+        {"name": "Inicio", "route": "home.index"},
+        {"name": "Categorías", "route": "home.categories"},
+        {"name": "Empaques y envoltorios", "route": "home.categories.c2"},
+    ]
+    return render(request, 'categories/pack.html', {"viewData": viewData})
+
+
+def c3(request):
+    viewData = {}
+    viewData["title"] = "Residuos no aprovechables"
+    viewData["breadcrumbItems"] = [
+        {"name": "Inicio", "route": "home.index"},
+        {"name": "Categorías", "route": "home.categories"},
+        {"name": "Residuos no aprovechables", "route": "home.categories.c3"},
+    ]
+    return render(request, 'categories/not-usable.html', {"viewData": viewData})
+
+
+def c4(request):
+    viewData = {}
+    viewData["title"] = "Orgánicos"
+    viewData["breadcrumbItems"] = [
+        {"name": "Inicio", "route": "home.index"},
+        {"name": "Categorías", "route": "home.categories"},
+        {"name": "Orgánicos", "route": "home.categories.c4"},
+    ]
+    return render(request, 'categories/organic.html', {"viewData": viewData})
