a8971a192eb0cd6184c8a1b20277568641c734ff
Daniel Correa <yo@danielgara.com>
2023-11-09 17:02:58 -0500

    fix: workflows

--- home/tests.py
+++ home/tests.py
@@ -7,9 +7,9 @@ class HomeTest(TestCase):
     def test_index_page(self):
         response = self.client.get(reverse('home.index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Save the world!")
+        self.assertContains(response, "Salva el Mundo!")
 
     def test_about_page(self):
         response = self.client.get(reverse('home.about'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "ABOUT US")
+        self.assertContains(response, "Acerca")
