763316d1f168c4ca51bd6952779f2d02309aebd3
Daniel Correa <yo@danielgara.com>
2023-11-10 11:37:00 -0500

    imp: camera scan

--- recycling/static/js/camera.js
+++ recycling/static/js/camera.js
@@ -1,126 +1,144 @@
-(() => {
-  // The width and height of the captured photo. We will set the
-  // width to the value defined here, but the height will be
-  // calculated based on the aspect ratio of the input stream.
+// The width and height of the captured photo. We will set the
+// width to the value defined here, but the height will be
+// calculated based on the aspect ratio of the input stream.
 
-  const width = 320; // We will scale the photo width to this
-  let height = 0; // This will be computed based on the input stream
+const width = 320; // We will scale the photo width to this
+let height = 0; // This will be computed based on the input stream
 
-  // |streaming| indicates whether or not we're currently streaming
-  // video from the camera. Obviously, we start at false.
+// |streaming| indicates whether or not we're currently streaming
+// video from the camera. Obviously, we start at false.
 
-  let streaming = false;
+let streaming = false;
 
-  // The various HTML elements we need to configure or control. These
-  // will be set by the startup() function.
+// The various HTML elements we need to configure or control. These
+// will be set by the startup() function.
 
-  let video = null;
-  let canvas = null;
-  let photo = null;
-  let startbutton = null;
+let video = null;
+let canvas = null;
+let photo = null;
+let startbutton = null;
 
-  function showViewLiveResultButton() {
-    if (window.self !== window.top) {
-      // Ensure that if our document is in a frame, we get the user
-      // to first open it in its own tab or window. Otherwise, it
-      // won't be able to request permission for camera access.
-      document.querySelector(".contentarea").remove();
-      const button = document.createElement("button");
-      button.textContent = "View live result of the example code above";
-      document.body.append(button);
-      button.addEventListener("click", () => window.open(location.href));
-      return true;
-    }
-    return false;
+function showViewLiveResultButton() {
+  if (window.self !== window.top) {
+    // Ensure that if our document is in a frame, we get the user
+    // to first open it in its own tab or window. Otherwise, it
+    // won't be able to request permission for camera access.
+    document.querySelector(".contentarea").remove();
+    const button = document.createElement("button");
+    button.textContent = "View live result of the example code above";
+    document.body.append(button);
+    button.addEventListener("click", () => window.open(location.href));
+    return true;
   }
+  return false;
+}
 
-  function startup() {
-    if (showViewLiveResultButton()) {
-      return;
-    }
-    video = document.getElementById("video");
-    canvas = document.getElementById("canvas");
-    photo = document.getElementById("photo");
-    startbutton = document.getElementById("startbutton");
+function startup() {
+  if (showViewLiveResultButton()) {
+    return;
+  }
+  video = document.getElementById("video");
+  canvas = document.getElementById("canvas");
+  photo = document.getElementById("photo");
+  startbutton = document.getElementById("startbutton");
 
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-        console.error(`An error occurred: ${err}`);
-      });
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(`An error occurred: ${err}`);
+    });
 
-    video.addEventListener(
-      "canplay",
-      (ev) => {
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth / width);
+  video.addEventListener(
+    "canplay",
+    (ev) => {
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth / width);
 
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
+        // Firefox currently has a bug where the height can't be read from
+        // the video, so we will make assumptions if this happens.
 
-          if (isNaN(height)) {
-            height = width / (4 / 3);
-          }
-
-          video.setAttribute("width", width);
-          video.setAttribute("height", height);
-          canvas.setAttribute("width", width);
-          canvas.setAttribute("height", height);
-          streaming = true;
+        if (isNaN(height)) {
+          height = width / (4 / 3);
         }
-      },
-      false,
-    );
 
-    startbutton.addEventListener(
-      "click",
-      (ev) => {
-        takepicture();
-        ev.preventDefault();
-      },
-      false,
-    );
+        video.setAttribute("width", width);
+        video.setAttribute("height", height);
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
+        streaming = true;
+      }
+    },
+    false,
+  );
 
-    clearphoto();
-  }
+  clearphoto();
+}
 
-  // Fill the photo with an indication that none has been
-  // captured.
+// Fill the photo with an indication that none has been
+// captured.
 
-  function clearphoto() {
-    const context = canvas.getContext("2d");
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+function clearphoto() {
+  const context = canvas.getContext("2d");
+  context.fillStyle = "#AAA";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  const data = canvas.toDataURL("image/png");
+  photo.setAttribute("src", data);
+}
+
+// Capture a photo by fetching the current contents of the video
+// and drawing it into a canvas, then converting that to a PNG
+// format data URL. By drawing it on an offscreen canvas and then
+// drawing that to the screen, we can change its size and/or apply
+// other changes before drawing it.
+
+function takepicture() {
+  const context = canvas.getContext("2d");
+  if (width && height) {
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
 
     const data = canvas.toDataURL("image/png");
     photo.setAttribute("src", data);
+  } else {
+    clearphoto();
   }
+}
 
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
+function scanpicture() {
+  $('#scanspinner').removeClass('d-none');
+  serverURL = ""
+  photoTaken = document.getElementById("photo");
+  imageData = photoTaken.getAttribute("src");
+  apikey = ""
+  $.ajax({
+    type: "POST",
+    url:serverURL,
+    data: JSON.stringify({'frame': imageData, 'user': 'online-guest'}),
+    crossDomain: true,
+    dataType: 'json',
+    headers: {"x-api-key": apikey},
+    success: function(response) {
+      responseElement = document.getElementById("scanresponse");
+      responseElement.innerHTML = response.prediction
+      $('#scanmodal').modal('show');
+      $('#scanspinner').addClass('d-none');
+    },
+    error: function(response) {
+      alert('Error escaneando foto')
+    },
+  });
+}
 
-  function takepicture() {
-    const context = canvas.getContext("2d");
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
+function closemodal() {
+  $('#scanmodal').modal('hide');
+}
 
-      const data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
-    } else {
-      clearphoto();
-    }
-  }
-
-  // Set up our event listener to run the startup process
-  // once loading is complete.
-  window.addEventListener("load", startup, false);
-})();
+// Set up our event listener to run the startup process
+// once loading is complete.
+window.addEventListener("load", startup, false);
