231e7ecd0c311c3c704ad525caad45474ed79cf5
Daniel Correa <yo@danielgara.com>
2023-12-21 15:35:29 -0500

    imp: SSL email

--- recycling/settings.py
+++ recycling/settings.py
@@ -129,6 +129,8 @@ CSRF_TRUSTED_ORIGINS = ['https://*.sasvar.co']
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER', default='')
+EMAIL_USE_SSL = True
